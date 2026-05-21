331d0e32751064522dd281870f7d28b11688a2d7
kheldae <taho.gougeon@gmail.com>
2026-05-21 10:37:38 +0200

    adding frees

--- service/nonogrammeservice.c
+++ service/nonogrammeservice.c
@@ -4,6 +4,8 @@
 
 #include "nonogrammeservice.h"
 
+#include <stdlib.h>
+
 unsigned char isNonogramBlackRowEqualsBlackColumn(const Nonogram *nonogram) {
   if (!nonogram)
     return 0;
@@ -31,41 +33,51 @@ unsigned char isNonogramBlackRowEqualsBlackColumn(const Nonogram *nonogram) {
   return blackRows == blackLines;
 }
 
-unsigned char isNonogramEachConstraintsLessOrEqualsThanDimension(const Nonogram *nonogram) {
-    unsigned char blackCount;
+unsigned char
+isNonogramEachConstraintsLessOrEqualsThanDimension(const Nonogram *nonogram) {
+  unsigned char blackCount;
 
-    if (nonogram==NULL) return 0;
+  if (nonogram == NULL)
+    return 0;
 
-    const unsigned char width = nonogramGetWidth(nonogram);
-    const unsigned char height = nonogramGetHeight(nonogram);
+  const unsigned char width = nonogramGetWidth(nonogram);
+  const unsigned char height = nonogramGetHeight(nonogram);
 
-    unsigned char *constraintArray;
-    for (unsigned char iRow = 0; iRow < height; iRow++) {
-        constraintArray = nonogramRowsConstraintsToArray(nonogram, iRow);
-        blackCount = 0;
-        for (unsigned char iCell = 0; iCell < nonogramRowsConstraintsGetSize(nonogram, iRow); iCell++) {
-            blackCount+=constraintArray[iCell];
-        }
-        if (iRow != 0) {
-            blackCount += nonogramRowsConstraintsGetSize(nonogram,iRow) - 1;
-        }
-        if (blackCount>width)return 0;
+  unsigned char *constraintArray;
+  for (unsigned char iRow = 0; iRow < height; iRow++) {
+    constraintArray = nonogramRowsConstraintsToArray(nonogram, iRow);
+    blackCount = 0;
+    for (unsigned char iCell = 0;
+         iCell < nonogramRowsConstraintsGetSize(nonogram, iRow); iCell++) {
+      blackCount += constraintArray[iCell];
     }
-    for (unsigned char iColumn = 0; iColumn < width; iColumn++) {
-        constraintArray = nonogramColumnsConstraintsToArray(nonogram, iColumn);
-        blackCount = 0;
-        for (unsigned char iCell = 0; iCell < nonogramColumnsConstraintsGetSize(nonogram, iColumn); iCell++) {
-            blackCount+=constraintArray[iCell];
-        }
-        if (iColumn != 0) {
-            blackCount += nonogramColumnsConstraintsGetSize(nonogram,iColumn) - 1;
-        }
-        if (blackCount>height)return 0;
+    if (iRow != 0) {
+      blackCount += nonogramRowsConstraintsGetSize(nonogram, iRow) - 1;
     }
+    if (blackCount > width)
+      return 0;
+    free(constraintArray);
+  }
+  for (unsigned char iColumn = 0; iColumn < width; iColumn++) {
+    constraintArray = nonogramColumnsConstraintsToArray(nonogram, iColumn);
+    blackCount = 0;
+    for (unsigned char iCell = 0;
+         iCell < nonogramColumnsConstraintsGetSize(nonogram, iColumn);
+         iCell++) {
+      blackCount += constraintArray[iCell];
+    }
+    if (iColumn != 0) {
+      blackCount += nonogramColumnsConstraintsGetSize(nonogram, iColumn) - 1;
+    }
+    if (blackCount > height)
+      return 0;
+    free(constraintArray);
+  }
 
-    return 1;
+  return 1;
 }
 
-unsigned char isNonogramConstraintsCountEqualsDimension(const Nonogram *nonogram) {
-    return 0;
+unsigned char
+isNonogramConstraintsCountEqualsDimension(const Nonogram *nonogram) {
+  return 0;
 }
