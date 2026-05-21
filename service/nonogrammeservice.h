ea709c49716df6bc44d61f8d739a746eaf043c0a
KillianRemy <killian.remy@student.junia.com>
2026-05-21 08:50:01 +0200

    guidelines ready

--- service/nonogrammeservice.h
+++ service/nonogrammeservice.h
@@ -5,4 +5,10 @@
 #ifndef NONOGRAMME_NONOGRAMMESERVICE_H
 #define NONOGRAMME_NONOGRAMMESERVICE_H
 
+#include "../entity/nonogram.h"
+
+unsigned char isNonogramBlackRowEqualsBlackColumn(const Nonogram* nonogram);
+unsigned char isNonogramEachConstraintsLessOrEqualsThanDimension(const Nonogram* nonogram);
+unsigned char isNonogramConstraintsCountEqualsDimension(const Nonogram* nonogram);
+
 #endif //NONOGRAMME_NONOGRAMMESERVICE_H
